4e6889b4bec247f30bd1f445ef6d5d9fe4e0f295
Andreas Merl <andreas.merl@student.tugraz.at>
2021-08-25 18:23:15 +0200

    now basic working task handling not all fields are implemented but the basics

--- resources/ews/task/ewsfetchtaskdetailjob.cpp
+++ resources/ews/task/ewsfetchtaskdetailjob.cpp
@@ -25,24 +25,24 @@ EwsFetchTaskDetailJob::EwsFetchTaskDetailJob(EwsClient &client, QObject *parent,
     shape << EwsPropertyField(QStringLiteral("task:ActualWork"));
     shape << EwsPropertyField(QStringLiteral("task:AssignedTime"));
     shape << EwsPropertyField(QStringLiteral("task:BillingInformation"));
-    //shape << EwsPropertyField(QStringLiteral("task:ChangeCount"));
+    shape << EwsPropertyField(QStringLiteral("task:ChangeCount"));
     shape << EwsPropertyField(QStringLiteral("task:Companies"));
     shape << EwsPropertyField(QStringLiteral("task:CompleteDate"));
-    shape << EwsPropertyField(QStringLiteral("task:Contacts"));
-    shape << EwsPropertyField(QStringLiteral("task:DelegationState"));
+    //shape << EwsPropertyField(QStringLiteral("task:Contacts"));
+    //shape << EwsPropertyField(QStringLiteral("task:DelegationState"));
     shape << EwsPropertyField(QStringLiteral("task:Delegator"));
-    //shape << EwsPropertyField(QStringLiteral("task:DueDate"));
+    shape << EwsPropertyField(QStringLiteral("task:DueDate"));
     shape << EwsPropertyField(QStringLiteral("task:IsAssignmentEditable"));
-    //shape << EwsPropertyField(QStringLiteral("task:IsComplete"));
-    //shape << EwsPropertyField(QStringLiteral("task:IsRecurring"));
+    shape << EwsPropertyField(QStringLiteral("task:IsComplete"));
+    shape << EwsPropertyField(QStringLiteral("task:IsRecurring"));
     shape << EwsPropertyField(QStringLiteral("task:IsTeamTask"));
-    shape << EwsPropertyField(QStringLiteral("task:Mileage"));
-    //shape << EwsPropertyField(QStringLiteral("task:Owner"));
-    //shape << EwsPropertyField(QStringLiteral("task:PercentComplete"));
-    shape << EwsPropertyField(QStringLiteral("task:Recurrence"));
+    //shape << EwsPropertyField(QStringLiteral("task:Mileage"));
+    shape << EwsPropertyField(QStringLiteral("task:Owner"));
+    shape << EwsPropertyField(QStringLiteral("task:PercentComplete"));
+    //shape << EwsPropertyField(QStringLiteral("task:Recurrence"));
     shape << EwsPropertyField(QStringLiteral("task:StartDate"));
     //shape << EwsPropertyField(QStringLiteral("task:Status"));
-    //shape << EwsPropertyField(QStringLiteral("task:StatusDescription"));
+    shape << EwsPropertyField(QStringLiteral("task:StatusDescription"));
     shape << EwsPropertyField(QStringLiteral("task:TotalWork"));
     mRequest->setItemShape(shape);
 }
@@ -71,28 +71,33 @@ void EwsFetchTaskDetailJob::processItems(const QList<EwsGetItemRequest::Response
         KCalendarCore::Todo *todo = new KCalendarCore::Todo();
         KCalendarCore::Todo::Ptr todoPtr;
         QString mimeContent = ewsItem[EwsItemFieldMimeContent].toString();
-        QString subject = ewsItem[EwsItemFieldSubject].toString();
-        QDateTime dtDue;
-        QDateTime dtStart;
-        //int percentComplete = ewsItem[EwsItemFieldPercentComplete].toInt();
-        //QString status = ewsItem[EwsItemFieldStatus].toString();
-        if(!ewsItem[EwsItemFieldDueDate].isNull())
+        QDateTime dtDue = ewsItem[EwsItemFieldDueDate].toDateTime();
+        QDateTime dtStart = ewsItem[EwsItemFieldStartDate].toDateTime();
+        QDateTime dtCompleted = ewsItem[EwsItemFieldCompleteDate].toDateTime();
+        if(!dtDue.isNull())
         {
-            todo->setDtDue(ewsItem[EwsItemFieldDueDate].toDateTime());
+            todo->setDtDue(dtDue);
         }
-        if(!ewsItem[EwsItemFieldStartDate].isNull())
+        if(!dtStart.isNull())
         {
-            todo->setDtStart(ewsItem[EwsItemFieldStartDate].toDateTime());
+            todo->setDtStart(dtStart);
         }
-
-        //todo->setPercentComplete(percentComplete);
-
         if(dtStart.isValid() && dtDue.isValid())
         {
             KCalendarCore::Duration duration = KCalendarCore::Duration(dtStart,dtDue,KCalendarCore::Duration::Type::Days);
             todo->setDuration(duration);
         }
-        todo->setSummary(subject);
+        if(!dtCompleted.isNull())
+        {
+            todo->setCompleted(dtCompleted);
+        }
+        else
+        {
+            todo->setCompleted(ewsItem[EwsItemFieldIsComplete].toBool());
+        }
+        todo->setSummary(ewsItem[EwsItemFieldSubject].toString());
+        todo->setPercentComplete(ewsItem[EwsItemFieldPercentComplete].toUInt());
+        todo->setOrganizer(ewsItem[EwsItemFieldOwner].toString());
         todoPtr = QSharedPointer<KCalendarCore::Todo>(todo);
         item.setPayload<KCalendarCore::Todo::Ptr>(todoPtr);
 
